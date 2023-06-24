03cb00a2a30a9bf0913235b6bd41adf77926d3e0
Santiago Alarcon <88556420+SantiagoAlarconDS@users.noreply.github.com>
2023-06-24 17:16:27 -0400

    Update info.py

--- classes/info.py
+++ classes/info.py
@@ -1,10 +1,16 @@
 class IDS:
     def __init__(self):
+
+        # Add Spotify API credentials of the APP
         self.spotify_client_id = 'XXXX'
         self.spotify_client_secret = 'XXXX'
+        
         # Can be a @username or a id, change this to your own @username or id for example.
         self.telegram_chat_id = 0000000000
+
+        # Add credentials from telegram API
         self.telegram_api_id = 'XXXX'
         self.telegram_api_hash = 'XXXX'
-        # change this to the token you get from @BotFather
+        
+        # Add the token that you get from @BotFather
         self.telegram_api_key = 'XXXX:XXXX-XXXX'
